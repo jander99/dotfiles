599583f11f70311558df95d356af1ce2c231bd8d
Jeff Anderson <jeffrey_d_anderson@homedepot.com>
2020-05-18 08:43:15 -0400

    New mac settings file

---
+++ .config/yadm/scripts/mac-settings.sh
@@ -0,0 +1,7 @@
+
+
+# Enabling full path in the Finder window title bar
+defaults write com.apple.finder _FXShowPosixPathInTitle -bool YES;
+
+# Disable the iCloud warning message in Settings
+defaults write com.apple.systempreferences AttentionPrefBundleIDs 0
